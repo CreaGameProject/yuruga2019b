44cf985ff837d7bdb996c7bd9449e507bae50cb0
Kino1213 <52032661+Kino1213@users.noreply.github.com>
2019-06-22 17:27:55 +0900

    範囲指定

--- Title scene/Assets/Scripts/kino/Player.cs
+++ Title scene/Assets/Scripts/kino/Player.cs
@@ -9,8 +9,14 @@ public class Player : MonoBehaviour
     public float speed;
     public float shootForce;
     public GameObject BulletsPrefab;
+
+    private Vector2 pos;
+    
+     
+
     // Start is called before the first frame update
-   
+  
+
     // Update is called once per frame
     void Update()
     {
@@ -26,6 +32,17 @@ public class Player : MonoBehaviour
             GameObject Bullets = Instantiate(BulletsPrefab, transform.position, transform.rotation) as GameObject;
             //Bullets.GetComponent<Rigidbody>().AddForce(Bullets.transform.forward * shootForce);
         }
-        
+       
+        Move(direction);
+
+
+    }
+    void Move(Vector2 direction)
+    {
+        Vector2 pos = transform.position;
+
+        pos.x = Mathf.Clamp(pos.x, -7.4f, -1.7f);
+        pos.y = Mathf.Clamp(pos.y, -4.3f, 4.3f);
+        transform.position = pos;
     }
 }
