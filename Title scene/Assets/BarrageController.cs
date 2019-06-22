783583ceeedb8b2230c9657d1c7a6b0ad75a882e
fujikawakaito <fujikawakaito@au.com>
2019-06-22 17:27:57 +0900

    エネミー弾幕

---
+++ Title scene/Assets/BarrageController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BarrageController : MonoBehaviour
+{
+    public  GameObject bullet;
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("Genbullet", 2f, 0.5f); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void Genbullet()
+    {
+        GameObject bulletIns = GameObject.Instantiate(bullet, transform.position, Quaternion.identity);
+        BulletController bulletInsComp = bulletIns.GetComponent<BulletController>();
+        bulletInsComp.enemy = gameObject;
+        bulletInsComp.player = GameObject.FindWithTag("Player");
+    }
+}
---
+++ Title scene/Assets/BulletController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletController : MonoBehaviour
+{
+    private float speed = 0.1f;
+    private Vector2 vec;
+    public GameObject player;
+    public GameObject enemy;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Vector2 enemypos = enemy.GetComponent<Transform>().position;
+        Vector2 playerpos = player.GetComponent<Transform>().position;
+        Vector2 diff = playerpos - enemypos;
+        float angle = Mathf.Atan2(diff.y, diff.x);
+        vec = new Vector2(Mathf.Cos(angle), Mathf.Sin(angle));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(vec * speed);
+        if (Mathf.Abs(transform.position.x) > 10 || Mathf.Abs(transform.position.y) > 7)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
